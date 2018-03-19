93c7b3201962b2f463e838bc51306295de3d635c
Jordan <murrayjordan96@gmail.com>
2018-03-19 14:42:59 -0400

    Update Memory.vhd

--- Memory.vhd
+++ Memory.vhd
@@ -3,16 +3,16 @@ LIBRARY IEEE;
     USE IEEE.STD_LOGIC_UNSIGNED.ALL;
     ENTITY MEMORY IS
         PORT(
-            ADDRESS : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-            DATAOUT : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-            DATAIN : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            address : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+            dataout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+            datain : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
             readwrite : IN STD_LOGIC;
             clk : in STD_LOGIC;
             rst : in STD_LOGIC
         );
     END ENTITY;
 
-    ARCHITECTURE BEV OF RAM IS
+    ARCHITECTURE BEV OF MEMORY IS
         TYPE MEM_2048 IS ARRAY (255 DOWNTO 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
         SIGNAL MEMORY : MEM_2048;
         SIGNAL ADDR : INTEGER RANGE 0 TO 255;
@@ -21,15 +21,16 @@ LIBRARY IEEE;
             BEGIN
                 IF(rising_edge(clk)) THEN
                     IF(rst='1') THEN
-                        DATAOUT <= "00000000";
+                        dataout <= "00000000";
                     ELSE
                         ADDR<=CONV_INTEGER(ADDRESS);
                         IF(readwrite='0')THEN
-                            MEMORY(ADDR)<=DATAIN;
+                            MEMORY(ADDR)<=datain;
                         ELSIF(readwrite='1')THEN
-                            DATAOUT<=MEMORY(ADDR);
-                            END IF;
+                            dataout<=MEMORY(ADDR);
+                        END IF;
                     END IF;
                 END IF;
             END PROCESS;
      END BEV;
+

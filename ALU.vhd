b6e18fafb1f962c460493c4f64282b6bc451fd3e
Jordan <murrayjordan96@gmail.com>
2018-03-21 14:12:51 -0400

    Set result to 8 bits

--- ALU.vhd
+++ ALU.vhd
@@ -28,7 +28,7 @@ entity ALU is
         A : in STD_LOGIC_VECTOR(7 downto 0);
         B : in STD_LOGIC_VECTOR(7 downto 0);
         aluop : in STD_LOGIC_VECTOR(3 downto 0);
-        result : out STD_LOGIC_VECTOR(15 downto 0);
+        result : out STD_LOGIC_VECTOR(7 downto 0);
     );
 end entity;
 

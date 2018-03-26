f9f0135c45ad370dd1a4097401d202c68376d15a
Jordan <Murrayjordan96@gmail.com>
2018-03-26 10:17:33 -0400

    Update assembler to write entire vhd file

---
+++ .src/mem_file_footer.vhd
@@ -0,0 +1,16 @@
+PROCESS(ADDRESS, DATAIN, readwrite, clk, rst)
+        BEGIN
+            IF(rising_edge(clk)) THEN
+                IF(rst='1') THEN
+                    dataout <= "00000000";
+                ELSE
+                    ADDR:=CONV_INTEGER(ADDRESS);
+                    IF(readwrite='1')THEN
+                        MEMORY(ADDR)<=datain;
+                    ELSE
+                        dataout<=MEMORY(ADDR);
+                    END IF;
+                END IF;
+            END IF;
+        END PROCESS;
+END BEV;
